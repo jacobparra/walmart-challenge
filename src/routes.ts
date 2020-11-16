8d9df1c968eeac412f4a365889fa7d4d811269d9
Jacob Parra <jacobrpg@gmail.com>
2020-11-16 03:54:48 -0300

    Removes unused Book model

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,9 @@
 import { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import apiSpec from '../openapi.json';
 
-import * as BookController from './controllers/book';
-
-const swaggerUiOptions = {
-  customCss: '.swagger-ui .topbar { display: none }'
-};
+//import * as ProductController from './controllers/product';
 
 const router = Router();
 
-// Book routes
-router.post('/book/add', BookController.add);
-router.get('/book/all', BookController.all);
-router.get('/book/search', BookController.search);
-
-// Dev routes
-if (process.env.NODE_ENV === 'development') {
-  router.use('/dev/api-docs', swaggerUi.serve);
-  router.get('/dev/api-docs', swaggerUi.setup(apiSpec, swaggerUiOptions));
-}
+//router.get('/', ProductController.search);
 
 export default router;
